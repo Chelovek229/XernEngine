0c35e2419613b0a4a07ca3735912abfc884bbcdc
Transistor <misteranthems5@gmail.com>
2026-08-25 16:04:13 +0500

    Refactor CollisionManager with add/remove methods
    Refactored CollisionManager to improve functionality and error handling.

--- src/core/physics/collisionManager.js
+++ src/core/physics/collisionManager.js
@@ -1,56 +1,61 @@
-// Менеджер столкновений
+// Collision Manager
 export class CollisionManager {
     constructor() {
-        if (!this || !(this instanceof CollisionManager)) {
-            throw new TypeError('Cannot call CollisionManager constructor as a function');
-        }
-
-        if (this.objects === null || typeof this.objects !== 'object') {
-            throw new TypeError('CollisionManager objects must be an array');
-        }
-
         this.objects = [];
     }
 
     addObject(obj) {
         if (obj === null || typeof obj !== 'object') {
-            throw new TypeError('CollisionManager objects must be an object');
+            throw new TypeError(
+                'CollisionManager: object must be an object'
+            );
         }
 
-        if (this.objects.indexOf(obj) !== -1) {
-            throw new Error('Object has already been added to CollisionManager');
+        if (this.objects.includes(obj)) {
+            throw new Error(
+                'CollisionManager: object already exists'
+            );
         }
 
-        try {
-            this.objects.push(obj);
-        } catch (error) {
-            throw new Error(`Error adding object to CollisionManager: ${error.message}`);
+        this.objects.push(obj);
+    }
+
+    removeObject(obj) {
+        const index = this.objects.indexOf(obj);
+
+        if (index === -1) {
+            throw new Error(
+                'CollisionManager: object not found'
+            );
         }
+
+        this.objects.splice(index, 1);
     }
 
     checkAllCollisions() {
-        try {
-            if (this.objects === null || typeof this.objects !== 'object' || !Array.isArray(this.objects)) {
-                throw new TypeError('CollisionManager objects must be an array');
-            }
+        for (let i = 0; i < this.objects.length; i++) {
+            const objA = this.objects[i];
 
-            for (let i = 0; i < this.objects.length; i++) {
-                const objA = this.objects[i];
-                if (objA === null || typeof objA !== 'object') {
-                    throw new TypeError('CollisionManager objects must be an object');
-                }
+            for (let j = i + 1; j < this.objects.length; j++) {
+                const objB = this.objects[j];
 
-                for (let j = i + 1; j < this.objects.length; j++) {
-                    const objB = this.objects[j];
-                    if (objB === null || typeof objB !== 'object') {
-                        throw new TypeError('CollisionManager objects must be an object');
-                    }
-
-                    // ...логика проверки столкновений...
+                if (this.checkCollision(objA, objB)) {
+                    this.onCollision(objA, objB);
                 }
             }
-        } catch (error) {
-            throw new Error(`Error checking all collisions in CollisionManager: ${error.message}`);
         }
+    }
+
+    checkCollision(a, b) {
+        return (
+            a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y
+        );
+    }
+
+    onCollision(a, b) {
+        console.log('Collision:', a, b);
     }
 }
